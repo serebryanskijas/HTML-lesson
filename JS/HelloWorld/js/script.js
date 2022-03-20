1a525252a85aeb767e36406c43c1da72cb419f3d
serebryanskijas <75270452+serebryanskijas@users.noreply.github.com>
2022-03-20 21:20:54 +0300

    Add files via upload

---
+++ JS/HelloWorld/js/script.js
@@ -0,0 +1,10 @@
+function helloConsole(){
+    console.log("Hello, world");
+}
+function helloAlert(){
+    alert("Hello, world");
+}
+function helloPage(){
+    var element = document.getElementById("container");
+    element.innerHTML+='<br>Hello, World';
+}
